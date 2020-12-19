753091fcaed59b67b464050f8576f76414e4ded6
Shivang Joshi <64350052+sj3214@users.noreply.github.com>
2020-12-19 15:31:07 +0400

    Add files via upload

--- js_file.js
+++ js_file.js
@@ -1,3 +1,11 @@
+var red, blue, green, black, white
+
+red = "#FF5050";
+blue = "#3BCCFF";
+green = "#37BF88";
+black = "#000000";
+white = "#FFFFFF";
+
 function new_img(identity, new_source){
     document.getElementById(identity).src=new_source
 }
@@ -8,4 +16,40 @@ function old_img(identity, old_source){
 
 function go_to_link(url){
     window.location.href = url;
+}
+
+function openTab(tabName, elmnt){
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display="none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.color=blue;
+        tablinks[i].style.fontWeight="normal";
+        tablinks[i].style.borderColor=white;
+    }
+ 
+    document.getElementById(tabName).style.display="block";
+    elmnt.style.color = red;
+    elmnt.style.fontWeight = "bold";
+    elmnt.style.borderColor = red;
+}
+
+function toggleSubTab(subTab, elmnt){  
+    subtab = document.getElementById(subTab);
+
+    if (subtab.style.display == "") {
+        subtab.style.display = "block";
+        elmnt.style.color = red;
+    }
+    else if (subtab.style.display == "block") {
+        subtab.style.display = "";            
+        elmnt.style.color = blue;
+    }
+
+
 }
